505e2bf9f113dab6f19e5bcbfa6fb9561d1c00a0
Nithin-KulangaraVeedu <61276392+Nithin-KulangaraVeedu@users.noreply.github.com>
2020-08-19 00:08:40 +0530

    Added files for Quote Generator

---
+++ script.js
@@ -0,0 +1,73 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuote = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Loader Show
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden =true;
+}
+//Hide Loading
+function complete(){
+    if(!loader.hidden){
+        quoteContainer.hidden =false;
+        loader.hidden =true;
+    }
+}
+
+
+//Get QUote from API
+
+async function getQuote(){
+    loading();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response= await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        //If Autho is blank add "Unknown" as author
+        if(data.quoteAuthor === ''){
+            authorText.innerText = "unknown";
+
+        }
+        else{
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        // Reduce font size for long quotes
+        if(data.quoteText.length >120){
+            quoteText.classList.add('long-quote');
+        }
+        else{
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        //console.log(data);
+        //Hide loader and show quote
+        complete();
+    }
+    catch(error){
+        getQuote();
+        //console.log('No quote', error);
+    }
+}
+
+function twitterQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl,'_blank');
+}
+//Event Listener
+newQuote.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',twitterQuote);
+
+
+//on Load
+
+getQuote();
+//loading();
